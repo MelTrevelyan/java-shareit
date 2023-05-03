45f813e830afe9ef6b64edd16c1eb0d7525dba53
MelTrevelyan <dtolstonogova@yandex.ru>
2023-05-03 18:35:45 +0700

    Added logging in ErrorHandler

--- src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -16,43 +16,49 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> handleUserValidationException(UserValidationException e) {
+        log.error(e.getMessage());
         return Map.of("Validation for user failed", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error(e.getMessage());
         return Map.of("Validation failed", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Map<String, String> handleUserNotFoundException(UserNotFoundException e) {
+        log.error(e.getMessage());
         return Map.of("Search for user failed", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Map<String, String> handleItemNotFoundException(ItemNotFoundException e) {
+        log.error(e.getMessage());
         return Map.of("Search for item failed", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public Map<String, String> handleNotOwnerForbiddenException(NotOwnerForbiddenException e) {
+        log.error(e.getMessage());
         return Map.of("User must be the owner", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
     public Map<String, String> handleEmailConflictException(EmailConflictException e) {
+        log.error(e.getMessage());
         return Map.of("Email conflict has occurred", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Map<String, String> handleUnknownException(Throwable e) {
-        log.error(e.toString());
+        log.error(e.getMessage());
         return Map.of("Unknown error has occurred", e.getMessage());
     }
 }
